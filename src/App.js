03e486d54f2ae9305d378a9fb95d22ab813c51df
Jefferson da Silva <je_jss@hotmail.com>
2020-07-28 19:48:54 -0300

    insert like function

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default function App() {
             <Text
               style={styles.likeText}
               // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
+              testID={`repository-likes-${repository.id}`}
             >
               3 curtidas
             </Text>
@@ -45,7 +45,7 @@ export default function App() {
             style={styles.button}
             onPress={() => handleLikeRepository(1)}
             // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
+            testID={`like-button-${repository.id}`}
           >
             <Text style={styles.buttonText}>Curtir</Text>
           </TouchableOpacity>
